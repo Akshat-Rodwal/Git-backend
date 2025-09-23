7f84f04fdd02808d3b3357b911319719d911916c
akshat <0808cs211019.ies@ipsacademy.org>
2025-09-23 16:55:22 +0530

    Akshat Rodwal in h1 tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-    return <div className="text-3xl font-bold underline"> Hello world!</div>;
+    return (
+        <>
+            <div className="text-3xl font-bold underline"> Hello world!</div>
+            <h1>Akshat Rodwal</h1>
+        </>
+    );
 }
 
 export default App;
